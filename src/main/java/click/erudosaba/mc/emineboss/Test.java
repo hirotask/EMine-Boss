ddb32661a52a72cf9263933650a08a6ed5b3a463
erudo <hashierudo@gmail.com>
2021-09-12 11:56:05 +0900

    ビルド設定の追加

--- src/main/java/click/erudosaba/mc/emineboss/Test.java
+++
@@ -1,4 +0,0 @@
-package click.erudosaba.mc.emineboss;
-
-public class Test {
-}
--- src/main/kotlin/click/erudosaba/mc/emineboss/Main.kt
+++ src/main/kotlin/click/erudosaba/mc/emineboss/Main.kt
@@ -1,4 +1,14 @@
 package click.erudosaba.mc.emineboss
 
-class Main {
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onDisable() {
+        logger.info("Plugin Disabled")
+    }
+
+    override fun onEnable() {
+        logger.info("Plugin Enabled")
+    }
 }
